517e052502299bef11c180aef0e2efb37c99701d
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-28 15:28:01 +0100

    Add files via upload

---
+++ Cwiczenia 6 zad 8/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Cwiczenia_6_zad_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string adres = "Cyryl Kwiecien\nul. Mokra 1/9\nTychy 40-321";
+            
+            Console.WriteLine(adres);
+
+            int i = 0;
+
+            do
+            {
+                if (adres[i] != '\n')
+                    
+                    Console.Write(adres[i]);
+                else
+                    Console.Write(" ");
+
+                i++;
+
+            }while (i < adres.Length);
+
+        }
+    }
+}
